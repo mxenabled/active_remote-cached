a5164628c1b6fafa577e5e137185969a1dfef9cb
Brandon Dewitt <brandonsdewitt@gmail.com>
2013-11-24 20:19:31 -0700

    add a delegator inteface to the cache

---
+++ lib/active_remote-cached/cache.rb
@@ -0,0 +1,26 @@
+module ActiveRemote::Cached
+  class Cache < ::SimpleDelegator
+    attr_reader :cache_provider
+
+    def initialize(new_cache_provider)
+      @cache_provider = new_cache_provider
+      validate_provider_method_present(:write)
+      validate_provider_method_present(:read)
+      validate_provider_method_present(:fetch)
+      
+      super(@cache_provider)
+    end
+
+    private
+
+    def validate_provider_method_present(method_name)
+      unless self.cache_provider.respond_to?(method_name)
+        raise <<-CACHE_METHOD
+          ActiveRemote::Cached::Cache must respond_to? #{method_name}
+          in order to be used as a caching interface for ActiveRemote
+        CACHE_METHOD
+      end
+    end
+      
+  end
+end
